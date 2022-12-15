8afa09d9f7a17ba49f3c50386e238c190226bd1c
SharOFF <sharov-pa@yandex.ru>
2022-12-15 15:34:59 +0300

    Task_047

---
+++ Task_047/Program.cs
@@ -0,0 +1,39 @@
+﻿// Task_047
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+string[] coord = Console.ReadLine().Split(" ");
+int row = int.Parse(coord[0]);
+int column = int.Parse(coord[1]);
+double[,] numbers = new double[row, column];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void FillArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)                          
+        {
+            array[i, j] = new Random().Next(-100, 100) / 10.0;
+        }
+    }
+}
+
+void PrintArray2D(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
